a9735a34fa554af194cfd749fed47c24fa0639f8
Adam Becvar <becvar.adam666@gmail.com>
2017-11-20 15:51:59 +0100

    Refactor CORS filters

--- src/main/java/org/cba/rest/cors/CorsRequestFilter.java
+++ src/main/java/org/cba/rest/cors/CorsRequestFilter.java
@@ -8,19 +8,17 @@ import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-//Comment out the two annotations below to disable CORS-handling
-@Provider  //This will ensure that the filter is used "automatically"
+@Provider
 @PreMatching
 public class CorsRequestFilter implements ContainerRequestFilter {
-  private final static Logger log = Logger.getLogger(CorsRequestFilter.class.getName());
-  @Override
-  public void filter(ContainerRequestContext requestCtx) throws IOException {
-    // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
-    if (requestCtx.getRequest().getMethod().equals("OPTIONS")) {
-      log.info("HTTP Method (OPTIONS) - Detected!");
-      // Just send a OK response back to the browser.
-      // The response goes through the chain of applicable response filters.
-      requestCtx.abortWith(Response.status(Response.Status.OK).build());
+
+    private final static Logger log = Logger.getLogger(CorsRequestFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext context) throws IOException {
+        if (context.getRequest().getMethod().equals("OPTIONS")) {
+            log.info("HTTP Method (OPTIONS) - Detected!");
+            context.abortWith(Response.status(Response.Status.OK).build());
+        }
     }
-  }
 } 
--- src/main/java/org/cba/rest/cors/CorsResponseFilter.java
+++ src/main/java/org/cba/rest/cors/CorsResponseFilter.java
@@ -8,18 +8,18 @@ import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-//Comment out the two annotations below to disable CORS-handling
 @Provider
 @PreMatching
 public class CorsResponseFilter implements ContainerResponseFilter {
-  private final static Logger LOG = Logger.getLogger(CorsResponseFilter.class.getName());
-  @Override
-  public void filter( ContainerRequestContext requestCtx, ContainerResponseContext res )
-    throws IOException {
-    LOG.info( "Executing REST response filter" );
-    res.getHeaders().add("Access-Control-Allow-Origin", "*" );
-    res.getHeaders().add("Access-Control-Allow-Credentials", "true" );
-    res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
-    res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
-  }
+
+    private final static Logger LOG = Logger.getLogger(CorsResponseFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestCtx, ContainerResponseContext res) throws IOException {
+        LOG.info("Executing REST response filter");
+        res.getHeaders().add("Access-Control-Allow-Origin", "*");
+        res.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
+    }
 }
